f288d2bd62dc35e90644aefdad3e284592f24095
Thomas Runyan <runyant13@gmail.com>
2025-06-23 17:34:13 -0400

    Added sql schema for first migration and installed necessary packages

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
-import _ "github.com/lib/pq"
+import 	_ "github.com/lib/pq"
 
 import (
 	"log"
 	"net/http"
+	"github.com/joho/godotenv"
 )
 
 type apiConfig struct {
@@ -15,7 +16,13 @@ type apiConfig struct {
 func main() {
 	log.Println("INFO: Gamelog Backend Server Started...")
 
-	// TODO Load ENV variables
+	godotenv.Load()
+	//dbURL := os.Getenv("DB_URL")
+	//db, err := sql.Open("postgres", dbURL)
+	//if err != nil {
+	//	log.Printf("%v", err)
+	//}
+
 	// TODO connect DB to query
 	// TODO Setup Api Config
 
