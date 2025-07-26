579897bc47524ac321b3665626f69a5809a2a526
Thomas Runyan <runyant13@gmail.com>
2025-07-26 17:20:52 -0400

    Added simple cli using cobra

---
+++ cmd/cli/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	var rootCmd = &cobra.Command{
+		Use: "cli",
+		Short: "gamelog-cli is an interactive way to use gamelog to track games",
+		Long: `gamelog-cli The use case for this right now is to use it to load IGDB
+			   info into the db.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Do Nothing")
+		},
+	}
+
+	var pullIGDB = &cobra.Command{
+		Use: "pullIGDB",
+		Short: "Command to pull information from relevant tables in IGDB",
+		Long: `Pulls information from the IGDB using apicalypse and stores it in
+		       our datebase to use.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Do Something")
+		},
+	}
+
+	rootCmd.AddCommand(pullIGDB)
+
+	rootCmd.Execute()
+	
+	// Execute()
+}
+
+
+
+// func Execute() {
+// 	for {
+// 		if err := rootCmd.Execute(); err != nil {
+// 			fmt.Fprintf(os.Stderr, err.Error())
+// 			os.Exit(1)
+// 		}
+// 	}
+// }
+
